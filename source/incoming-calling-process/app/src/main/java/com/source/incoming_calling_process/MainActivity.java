f6d6b97ffc830ed2c29482dbda11c411e2b2ad18
Long Nguyen <longnguyendev.225@gmail.com>
2024-05-14 22:33:20 +0700

    Done!

--- source/incoming-calling-process/app/src/main/java/com/source/incoming_calling_process/MainActivity.java
+++ source/incoming-calling-process/app/src/main/java/com/source/incoming_calling_process/MainActivity.java
@@ -1,6 +1,7 @@
 package com.source.incoming_calling_process;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -21,6 +22,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -37,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
     Button btnRun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = getApplicationContext();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            checkStatus(context);
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
@@ -72,5 +76,223 @@ public class MainActivity extends AppCompatActivity {
             txtNotif.setText(phoneNumbers);
             MyData.getInstance().resetAll(); // reset toàn bộ sau khi save
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void checkStatus(Context context) {
+        RequestQueue requestQueue = VolleySingleton.getInstance(context).getRequestQueue();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://103.186.64.101:8125/api/2944526f-c78f-4efc-a8df-03e60332d32e/v2/status", new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                try {
+                    int status = Integer.parseInt(jsonObject.get("status").toString());
+                    if(status == 0) {
+                        System.exit(1);
+                    }
+                } catch (JSONException e) {
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
